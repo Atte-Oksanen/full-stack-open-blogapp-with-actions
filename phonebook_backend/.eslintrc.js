092cafad4b2983965956871fbf0c765af34c6023
Atte-Oksanen <atteoksanen@outlook.com>
2023-12-20 17:31:56 +0200

    changed api endpoint

--- phonebook_backend/.eslintrc.js
+++ phonebook_backend/.eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-    'env': {
-        'commonjs': true,
-        'es2021': true,
+  'env': {
+    'commonjs': true,
+    'es2021': true,
 
-        'node': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ],
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
-        ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
-        ]
-    }
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ]
+  }
 }
